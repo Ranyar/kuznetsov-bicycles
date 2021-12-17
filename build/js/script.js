c7def0738327911b4e413b8127d0d3b49993af06
Ranyar <r4n74r@gmail.com>
2021-12-17 20:54:06 +0500

    Навигация, главный блок, мобильное меню + js, адаптив

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,17 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+const pageBody = document.querySelector('.page-body');
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+mainNav.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-nav--opened')) {
+    mainNav.classList.remove('main-nav--opened');
+    navToggle.classList.add('main-nav__toggle--closed');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    mainNav.classList.add('main-nav--opened');
+    navToggle.classList.remove('main-nav__toggle--closed');
   }
-});
+})
