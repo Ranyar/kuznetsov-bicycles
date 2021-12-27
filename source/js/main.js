28faba214a5c54041b8b7b0dec173acd7338d7ac
Ranyar <r4n74r@gmail.com>
2021-12-27 10:08:53 +0500

    Финальные правки после тестирования

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@
 
 const pageBody = document.querySelector('.page-body');
 const mainNav = document.querySelector('.main-nav');
+const mainNavItems = document.querySelectorAll('.main-nav__item');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 mainNav.classList.remove('main-nav--nojs');
@@ -18,6 +19,15 @@ navToggle.addEventListener('click', function () {
   }
 });
 
+for (let i = 0; i < mainNavItems.length; i++) {
+  mainNavItems[i].addEventListener('click', function () {
+    if (mainNav.classList.contains('main-nav--opened')) {
+      pageBody.classList.remove('page-body--lock');
+      mainNav.classList.remove('main-nav--opened');
+      navToggle.classList.add('main-nav__toggle--closed');
+    }
+  });
+}
 
 //----- local storage для формы
 
