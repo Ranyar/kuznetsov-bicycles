8d5c0e436ffd25c5e32827ff92ae89b0c4badc47
Ranyar <r4n74r@gmail.com>
2022-01-05 10:26:25 +0000

    Deploying to gh-pages from @ Ranyar/kuznetsov-bicycles@eb326bdbec18f3e3a061cea06b90cad9e6c3a125 🚀

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const pageBody = document.querySelector('.page-body');
+const mainNav = document.querySelector('.main-nav');
+const mainNavItems = document.querySelectorAll('.main-nav__item');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+mainNav.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-nav--opened')) {
+    pageBody.classList.remove('page-body--lock');
+    mainNav.classList.remove('main-nav--opened');
+    navToggle.classList.add('main-nav__toggle--closed');
+  } else {
+    mainNav.classList.add('main-nav--opened');
+    pageBody.classList.add('page-body--lock');
+    navToggle.classList.remove('main-nav__toggle--closed');
+  }
+});
+
+for (let i = 0; i < mainNavItems.length; i++) {
+  mainNavItems[i].addEventListener('click', function () {
+    if (mainNav.classList.contains('main-nav--opened')) {
+      pageBody.classList.remove('page-body--lock');
+      mainNav.classList.remove('main-nav--opened');
+      navToggle.classList.add('main-nav__toggle--closed');
+    }
+  });
+}
+
+//----- local storage для формы
+
+const orderNameInput = document.getElementById('order-name');
+const orderPhoneInput = document.getElementById('order-phone');
+
+orderNameInput.value = localStorage.getItem('order-name');
+orderPhoneInput.value = localStorage.getItem('order-phone');
+
+orderNameInput.addEventListener('input', () => {
+  localStorage.setItem('order-name', orderNameInput.value);
+})
+
+orderPhoneInput.addEventListener('input', () => {
+  localStorage.setItem('order-phone', orderPhoneInput.value);
+})
